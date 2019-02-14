020a954fcd1ef64fc89c10c6fc89d3f599ff1b37
Carole Douchy <carole.douchy@ehl.ch>
2019-02-14 15:35:27 +0100

    all methods working

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   resources :cocktails, only: [:index, :new, :create, :show] do
     resources :doses, only: [:new, :create, :destroy]
   end
-  resources :doses, only: [:show]
 end
