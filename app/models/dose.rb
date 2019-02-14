c5bebf7015ab6f18463a449dd95be67e43e523cc
Carole Douchy <carole.douchy@ehl.ch>
2019-02-14 10:59:16 +0100

    Cocktail, Ingredient, Dose models and DB done

---
+++ app/models/dose.rb
@@ -0,0 +1,8 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+
+  validates :description, :cocktail, :ingredient, presence: true
+  validates_uniqueness_of :cocktail, :scope => :ingredient
+
+end
